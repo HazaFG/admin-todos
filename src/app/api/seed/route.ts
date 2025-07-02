5ecf197e411edcfc1e91fd33932ff0418ba8a6ae
HazaFG <hazaelfg@gmail.com>
2025-07-02 16:41:32 -0700

    creando nuestro seeder de gemas del infinito, turbopack disabled

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -2,14 +2,26 @@ import prisma from "@/lib/prisma"
 import { NextResponse } from "next/server"
 
 export async function GET(request: Request) {
-  //hay que hacer una insercion en la base de datos
 
-  //preparar insercion este prisma es el que usaste en src/lib/prisma.ts
-  const todo = await prisma.todo.create({
-    //la data es basicamente la informacion que yo quiero insertar en un todo
-    data: { description: 'Piedra del alma' }
+  await prisma.todo.deleteMany()
+
+  await prisma.todo.createMany({
+    data: [
+      { description: 'Piedra del alma', complete: true },
+      { description: 'Piedra del poder' },
+      { description: 'Piedra del tiempo' },
+      { description: 'Piedra del espacio' },
+      { description: 'Piedra de la realidad' },
+    ]
   })
-  //
+
+  //hay que hacer una insercion en la base de datos
+  //preparar insercion este prisma es el que usaste en src/lib/prisma.ts
+  // const todo = await prisma.todo.create({
+  //   //la data es basicamente la informacion que yo quiero insertar en un todo
+  //   data: { description: 'Piedra del alma' }
+  // })
+
   // console.log(todo)
 
   return NextResponse.json({
