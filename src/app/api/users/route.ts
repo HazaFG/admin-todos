9ce3a13d9f35e04ca0a0e14157cc22a040714315
HazaFG <hazaelfg@gmail.com>
2025-07-02 23:55:39 -0700

    paginacion basica con console log

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -3,13 +3,31 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
 
-  const usuario = await prisma.users.create({
-    data: { name: 'Hazael', description: 'es pro' }
+  await prisma.users.deleteMany()
+
+  const usuario = await prisma.users.createMany({
+    data: [
+      { name: 'Hazael', description: 'es pro' },
+      { name: 'Andrea', description: 'Es hermosisima' },
+      { name: 'Abdiel', description: 'e una rata sucia' },
+      { name: 'Juan pepe', description: 'es pro' },
+      { name: 'Etesech', description: 'es pro' },
+      { name: 'Popo de burro', description: 'equis de' },
+      { name: 'yon', description: 'asdfasdf' },
+      { name: 'uriel', description: 'no se xd' },
+      { name: 'papayu', description: 'es otra rata' },
+    ]
   })
 
-  console.log(usuario)
+  const results = await prisma.users.findMany({
+    skip: 3,
+    take: 1,
+  })
+
+  console.log('el resultado de la busqueda es', results)
+  // console.log(usuario)
 
   return NextResponse.json({
-    user: 'El usuario Hazael ha sido creado'
+    user: 'Los usuarios han sido creados'
   })
 }
