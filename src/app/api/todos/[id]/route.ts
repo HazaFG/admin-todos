c8dd88167b932d34762933a5abfddc5d8d596e3d
HazaFG <hazaelfg@gmail.com>
2025-07-11 10:49:28 -0700

    Actualizar todos by id - PUT

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -17,9 +17,34 @@ export async function GET(request: Request, { params }: Segments) {
   if (!todo) {
     return NextResponse.json(
       { message: `Todo con id ${id} no existe` },
-      { status: 404 }// bad request
+      { status: 404 }// not found
     )
   }
 
   return NextResponse.json(todo)
 }
+
+export async function PUT(request: Request, { params }: Segments) {
+  //esto es destructuracion
+  const { id } = params;
+  const todo = await prisma.todo.findFirst({ where: { id } })
+  console.log('este es el todo', todo)
+
+  if (!todo) {
+    return NextResponse.json(
+      { message: `Todo con id ${id} no existe` },
+      { status: 404 }// not found
+    )
+  }
+
+  const body = await request.json()
+
+  const updatedTodo = await prisma.todo.update({
+    where: { id },
+    data: { ...body }
+  })
+
+  return NextResponse.json(updatedTodo)
+}
+
+
