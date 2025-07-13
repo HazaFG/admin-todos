c7a17a813037b3ef47dadff93ad057b050d6fbf8
HazaFG <hazaelfg@gmail.com>
2025-07-13 00:51:51 -0700

    mostrar todos de la base de datos asi raw sin ningun tipo de diseno bonito jaja

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,7 +1,20 @@
-export default function RestTodosPage() {
+import prisma from "@/lib/prisma"
+
+export const metadata = {
+  title: 'Listado de todos',
+  description: 'SEO title',
+}
+
+export default async function RestTodosPage() {
+  //Aqui vamos a precargar los todos ya que ya los tenemos en prisma, es decir, desde nuestra base de datos
+  const todos = await prisma.todo.findMany({ orderBy: { description: 'asc' } })
+
   return (
     <>
       <h1>HOla, soy la rest-todos page</h1>
+      {
+        JSON.stringify(todos)
+      }
     </>
   )
 }
