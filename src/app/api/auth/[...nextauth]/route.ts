43322afba9504c85efb943d53ca66058138ebf50
HazaFG <hazaelfg@gmail.com>
2025-07-29 18:06:51 -0700

    next-auth agregado, primer endpoint para autenticacion, de momento solo github, Introduccion a seccion sobre autenticacoin

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,25 @@
+import NextAuth, { NextAuthOptions } from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+export const authOptions: NextAuthOptions = {
+
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID ?? '',
+      clientSecret: process.env.GITHUB_SECRET ?? '',
+    }),
+    // ...add more providers here
+  ],
+}
+
+const handler = NextAuth(authOptions);
+
+//Al exportar esto asi, estamos haciendo lo mismo que con los otros endpoints que tienen su POST y GET, ya que asi es como los detecta nextjs, asi es como los maneja sus endpoints
+export { handler as GET, handler as POST }
+
+// export default NextAuth(authOptions)
+
+
+
+
+
